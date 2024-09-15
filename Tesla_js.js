bcb4f5d71e89b276543d1ba1ab0cb922cb0e6181
Line SON <lineson@Lineui-MacBookAir.local>
2024-09-16 05:29:43 +0900

    css추가 직전까지 진행_240916

--- Tesla_js.js
+++ Tesla_js.js
@@ -1,22 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // 변수 선언
   const whitebox = document.querySelector(".whitebox");
   const menuItems = document.querySelectorAll(".menubox > li");
   const headerHeight = 56;
-  const hiddenHeader = document.querySelector(".mainheader");
+  const mainHeader = document.querySelector(".mainheader");
   const logo = document.querySelector(".logobox a > img");
   const menuSpans = document.querySelectorAll(".menubox > li > button > span");
   const utilIcons = document.querySelectorAll(".utilnav > li > a > img");
-  const mainHeader = document.querySelector(".mainheader");
   const logobox = document.querySelector(".logobox");
   const utilbox = document.querySelector(".utilbox");
   const blurscreen = document.querySelector(".blurscreen");
-  const openMenu = document.querySelector(".openmenu");
-  const mobileMenu = document.querySelector(".mobilemenu");
   const body = document.querySelector("body");
 
   let lastScrollY = window.scrollY;
   let activeSubmenu = null;
 
+  // 헤더 색상 설정 함수
   function setHeaderColors(color) {
     const filterValue = color === "black" ? "brightness(0) invert(0)" : "brightness(0) invert(1)";
     logo.style.filter = filterValue;
@@ -24,215 +23,155 @@ document.addEventListener("DOMContentLoaded", function () {
     utilIcons.forEach((icon) => (icon.style.filter = filterValue));
   }
 
-  function closeWhitebox() {
-    if (window.scrollY === 0) {
-      whitebox.classList.remove("open");
-      hiddenHeader.classList.remove("scrolled");
-      setHeaderColors("white");
-      whitebox.style.height = `${headerHeight}px`;
-    } else {
-      whitebox.style.height = `${headerHeight}px`;
-      setHeaderColors("black");
-      hiddenHeader.classList.add("scrolled");
-    }
-
-    if (activeSubmenu) {
-      activeSubmenu.style.height = "0px";
-      activeSubmenu.classList.remove("show");
-      activeSubmenu = null;
-      hideBlurScreen();
-    }
-  }
-
-  function openWhitebox() {
+  // 화이트박스 열기 함수
+  function openWhitebox(height) {
     whitebox.classList.add("open");
-    hiddenHeader.classList.add("scrolled");
+    whitebox.style.height = `${height}px`;
+    mainHeader.classList.add("scrolled");
     setHeaderColors("black");
   }
 
+  // 화이트박스 닫기 함수
+  function closeWhitebox() {
+    whitebox.classList.remove("open");
+    whitebox.style.height = `${headerHeight}px`;
+    if (window.scrollY === 0) {
+      mainHeader.classList.remove("scrolled");
+      setHeaderColors("white");
+    }
+  }
+
+  // 블러 스크린 표시 함수
   function showBlurScreen() {
     if (window.innerWidth < 1200) return;
     blurscreen.style.display = "block";
   }
 
+  // 블러 스크린 숨기기 함수
   function hideBlurScreen() {
     blurscreen.style.display = "none";
   }
 
-  // 모바일 메뉴 열기 및 스크롤 비활성화
-  openMenu.addEventListener("click", function () {
-    if (mobileMenu.classList.contains("open")) {
-      mobileMenu.classList.remove("open");
-      body.style.overflow = ""; // 스크롤 활성화
-      hideBlurScreen();
-    } else {
-      mobileMenu.classList.add("open");
-      body.style.overflow = "hidden"; // 스크롤 비활성화
-      showBlurScreen();
-    }
+  // 메뉴 아이템에 마우스 진입 시 이벤트 핸들러
+  menuItems.forEach(function (item) {
+    item.addEventListener("mouseenter", function () {
+      let submenu = item.querySelector(".submenu");
+      let whiteboxHeight = headerHeight;
 
-    // 모바일 메뉴 닫기 및 스크롤 활성화
-    const closeMenu = document.querySelector(".closemenu");
-    closeMenu.addEventListener("click", function () {
-      mobileMenu.classList.remove("open");
-      body.style.overflow = ""; // 스크롤 활성화
-    });
-  });
+      if (submenu) {
+        if (item.classList.contains("vehicles")) {
+          whiteboxHeight = 552;
+        } else if (item.classList.contains("charging") || item.classList.contains("discover")) {
+          whiteboxHeight = 360;
+        } else if (item.classList.contains("shop")) {
+          whiteboxHeight = 336;
+        }
 
-  // 차량, 충전, 살펴보기 버튼 클릭 시 show 클래스 추가 및 mobilesubheader 생성
+        // 기존에 열려있는 서브메뉴를 닫기
+        if (activeSubmenu && activeSubmenu !== submenu) {
+          activeSubmenu.classList.remove("show");
+          activeSubmenu.style.height = "0px";
+        }
 
-  const mobileMenuButtons = document.querySelectorAll(
-    ".mobilevehicles button, .mobilecharging button, .mobilediscover button"
-  );
-
-  mobileMenuButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      const parentLi = button.closest("li");
-      const submenu = parentLi.querySelector(".mobile");
-
-      if (submenu && !submenu.classList.contains("show")) {
+        openWhitebox(whiteboxHeight);
         submenu.classList.add("show");
+        submenu.style.height = `${whiteboxHeight - headerHeight}px`;
+        activeSubmenu = submenu;
+        showBlurScreen();
+      }
+    });
 
-        const mobilesubheader = document.createElement("div");
-        mobilesubheader.classList.add("mobilesubheader");
-
-        const backButton = document.createElement("button");
-        backButton.innerHTML = `<img src="./img/mobile/뒤로가기버튼.svg" alt="" />뒤로가기버튼`;
-        backButton.classList.add("back-button");
-
-        const title = document.createElement("h2");
-        title.innerText = button.innerText;
-
-        mobilesubheader.appendChild(backButton);
-        mobilesubheader.appendChild(title);
-
-        submenu.insertBefore(mobilesubheader, submenu.firstChild);
-
-        backButton.addEventListener("click", function () {
-          submenu.classList.remove("show");
-          submenu.removeChild(mobilesubheader);
-        });
+    item.addEventListener("mouseleave", function (event) {
+      const relatedTarget = event.relatedTarget;
+      if (
+        !mainHeader.contains(relatedTarget) &&
+        !whitebox.contains(relatedTarget) &&
+        !menuItems.some((li) => li.contains(relatedTarget))
+      ) {
+        if (activeSubmenu) {
+          activeSubmenu.classList.remove("show");
+          activeSubmenu.style.height = "0px";
+          activeSubmenu = null;
+        }
+        closeWhitebox();
+        hideBlurScreen();
       }
     });
   });
 
+  // 로고박스 및 유틸박스에 마우스 진입 시 화이트박스 제거
+  [logobox, utilbox].forEach((element) => {
+    element.addEventListener("mouseenter", function () {
+      if (activeSubmenu) {
+        activeSubmenu.classList.remove("show");
+        activeSubmenu.style.height = "0px";
+        activeSubmenu = null;
+      }
+      closeWhitebox();
+      hideBlurScreen();
+    });
+  });
+
+  // 서브메뉴 영역에서 마우스 이탈 시 서브메뉴만 닫기
+  document.querySelectorAll(".submenu").forEach((submenu) => {
+    submenu.addEventListener("mouseleave", function (event) {
+      const relatedTarget = event.relatedTarget;
+      if (
+        !mainHeader.contains(relatedTarget) &&
+        !submenu.contains(relatedTarget) &&
+        !menuItems.some((li) => li.contains(relatedTarget))
+      ) {
+        submenu.classList.remove("show");
+        submenu.style.height = "0px";
+        activeSubmenu = null;
+        if (window.scrollY === 0) {
+          closeWhitebox();
+        } else {
+          whitebox.style.height = `${headerHeight}px`;
+        }
+        hideBlurScreen();
+      }
+    });
+  });
+
+  // 스크롤 이벤트 핸들러
   window.addEventListener("scroll", function () {
     const currentScrollY = window.scrollY;
 
-    // 스크롤 제한: cybertruck div까지만 스크롤 가능
-    limitScrollToCybertruck();
-
-    if (currentScrollY < lastScrollY && currentScrollY > 0) {
-      openWhitebox();
-      whitebox.style.height = `${headerHeight}px`;
-      hiddenHeader.classList.remove("hidden");
-      hiddenHeader.classList.add("show");
-    } else if (currentScrollY > lastScrollY) {
+    if (currentScrollY > lastScrollY) {
+      // 스크롤 다운: 헤더 제거
+      mainHeader.classList.add("hidden");
+      if (activeSubmenu) {
+        activeSubmenu.classList.remove("show");
+        activeSubmenu.style.height = "0px";
+        activeSubmenu = null;
+      }
       closeWhitebox();
-      hiddenHeader.classList.remove("show");
-      hiddenHeader.classList.add("hidden");
+      hideBlurScreen();
+    } else {
+      // 스크롤 업: 화이트박스와 헤더 노출
+      mainHeader.classList.remove("hidden");
+      mainHeader.classList.add("scrolled");
+      setHeaderColors("black");
+      whitebox.classList.add("open");
+      whitebox.style.height = `${headerHeight}px`;
     }
 
     if (currentScrollY === 0) {
+      // 최상단 도달: 화이트박스 제거
       closeWhitebox();
-      hiddenHeader.classList.add("show");
-      hiddenHeader.classList.remove("hidden");
+      mainHeader.classList.remove("scrolled");
+      setHeaderColors("white");
     }
 
     lastScrollY = currentScrollY;
   });
 
-  menuItems.forEach(function (item) {
-    item.addEventListener("mouseenter", function () {
-      openWhitebox();
-      showBlurScreen();
-
-      let whiteboxHeight;
-      if (item.classList.contains("vehicles")) {
-        whiteboxHeight = 552;
-      } else if (item.classList.contains("charging") || item.classList.contains("discover")) {
-        whiteboxHeight = 360;
-      } else if (item.classList.contains("shop")) {
-        whiteboxHeight = 336;
-      }
-
-      whitebox.style.height = `${whiteboxHeight}px`;
-
-      if (activeSubmenu && activeSubmenu !== item.querySelector(".submenu")) {
-        activeSubmenu.style.height = "0px";
-        activeSubmenu.classList.remove("show");
-      }
-
-      const submenu = item.querySelector(".submenu");
-      if (submenu) {
-        submenu.style.height = `${whiteboxHeight - headerHeight}px`;
-        submenu.classList.add("show");
-        activeSubmenu = submenu;
-      }
-    });
-
-    item.addEventListener("mouseleave", function (event) {
-      const isInHeader = mainHeader.contains(event.relatedTarget);
-      const isInLogobox = logobox.contains(event.relatedTarget);
-      const isInUtilbox = utilbox.contains(event.relatedTarget);
-      const isInAnotherMenu = Array.from(menuItems).some((li) => li.contains(event.relatedTarget));
-
-      if (!isInAnotherMenu && !activeSubmenu) {
-        hideBlurScreen();
-      }
-
-      if (isInLogobox || isInUtilbox) {
-        if (window.scrollY === 0) {
-          closeWhitebox();
-        } else {
-          if (activeSubmenu) {
-            activeSubmenu.style.height = "0px";
-            activeSubmenu.classList.remove("show");
-            activeSubmenu = null;
-          }
-          whitebox.style.height = `${headerHeight}px`;
-        }
-      } else if (!isInHeader && !isInAnotherMenu) {
-        closeWhitebox();
-      }
-    });
-  });
-
-  logobox.addEventListener("mouseenter", function () {
-    hideBlurScreen();
-    if (window.scrollY === 0) {
-      closeWhitebox();
-    } else {
-      if (activeSubmenu) {
-        activeSubmenu.style.height = "0px";
-        activeSubmenu.classList.remove("show");
-        activeSubmenu = null;
-      }
-      whitebox.style.height = `${headerHeight}px`;
-    }
-  });
-
-  utilbox.addEventListener("mouseenter", function () {
-    hideBlurScreen();
-    if (window.scrollY === 0) {
-      closeWhitebox();
-    } else {
-      if (activeSubmenu) {
-        activeSubmenu.style.height = "0px";
-        activeSubmenu.classList.remove("show");
-        activeSubmenu = null;
-      }
-      whitebox.style.height = `${headerHeight}px`;
-    }
-  });
-
+  // 메인헤더에 마우스 진입 시
   mainHeader.addEventListener("mouseenter", function () {
-    if (whitebox.classList.contains("open")) {
-      whitebox.style.height = headerHeight + "px";
+    if (window.scrollY === 0 && !activeSubmenu) {
+      closeWhitebox();
+      setHeaderColors("white");
     }
-  });
-
-  mainHeader.addEventListener("mouseleave", function () {
-    closeWhitebox();
   });
 });
